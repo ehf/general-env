f8486e5ac02446df1e19e8691601d0121e395f92
ehf <1011013+ehf@users.noreply.github.com>
2022-07-04 17:58:00 -0700

    Update py-venv.sh

--- ansible/py-venv.sh
+++ ansible/py-venv.sh
@@ -1,15 +1,18 @@
 # reference: https://docs.python.org/3/library/venv.html#module-venv
-# source me
+#
+# source ./py-env.sh
+#
 export PATH=/opt/homebrew/bin:$PATH
 venv_dir="${VENVDIR:-"$HOME/.venvs/$(basename $(pwd))"}"
 mkdir -p "$venv_dir"
 
-if [ ! -f "${venv_dir}/bin/activate" ]
-then
-  python3 -m venv "$venv_dir"
-fi
+test ! -f "${venv_dir}/bin/activate" || python3 -m venv "$venv_dir"
 
 source "${venv_dir}/bin/activate"
 python3 -m pip install pip==22.1.1
 
 python3 -m pip install -r requirements.txt
+ansible-galaxy collection install -r requirements.yml
+ansible-galaxy install -r requirements.yml
+
+##
